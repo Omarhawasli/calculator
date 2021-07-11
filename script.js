ba6527c65a7383ab2d97b168bd52dc5df155311f
Omar <amr.hawasli.1@gmail.com>
2021-07-11 11:21:55 +0200

    fixing switch statement and constant output and history

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const getHistory = () => {
     return document.getElementById("history-value").innerText;
 }
 const printHistroy = (num) => {
-    return document.getElementById("history-value").innerText=num;
+    return document.getElementById("history-value").innerText = num;
 }
 const getOutput = () => {
     return document.getElementById("output-value").innerText;
 }
 const printOutput = (num) => {
     if(num === ""){
-        document.getElementById("output-value").innerText=(num);
+        document.getElementById("output-value").innerText = (num);
     }else{
-        document.getElementById("output-value").innerText=getFormattedNumber(num);
+        document.getElementById("output-value").innerText = getFormattedNumber(num);
 
     }
 }
@@ -23,52 +23,64 @@ const getFormattedNumber = (num) => {
 }
 printOutput("")
 
-let reverseNumberFormat = (num) => {
+const reverseNumberFormat = (num) => {
     return Number(num.replace(/,/g,''))
 }
 
-let operator = document.getElementsByClassName("operator")
+const operator = document.getElementsByClassName("operator")
 console.log("operator", typeof(operator));
+
+
+const number = document.querySelectorAll(".number")
+number.forEach(item => {
+    item.addEventListener('click',function(){
+        let output = reverseNumberFormat(getOutput())
+        if(output !== NaN){
+            output = output + this.id
+            printOutput(output)
+        }
+    })
+})
 
 document.querySelectorAll('.operator').forEach(function(item) {
     item.addEventListener('click',function(){
+        const output = getOutput();
+        const history = getHistory();
         switch(this.id) {
             case "clear":
                 printHistroy("");
                 printOutput("");
               break;
             case "backspace":
-                var output = reverseNumberFormat(getOutput()).toString()
+                const outputS = reverseNumberFormat(getOutput()).toString()
                 if(output){
-                    output = output.substr(0, output.length-1);
+                    output = outputS.substr(0, output.length-1);
                     printOutput(output)}
                 break;
             case "+": 
-                var output = getOutput()
-                var history = getHistory()
-                printHistroy(`${output} + ${history}`)
+                if(output){
+                    printHistroy(`${output} + ${history}`)
                     printOutput("")
                     console.log("plus");
+                }
               break;
             case "-":
-                var output = getOutput()
-                var history = getHistory()
-                printHistroy(`${output} - ${history}`)
-                printOutput("")
+                if(output){
+                    printHistroy(`${output} - ${history}`)
+                    printOutput("")
+                }
             break;
             case "*":
-                var output = getOutput()
-                var history = getHistory()
-                printHistroy(`${output} * ${history}`)
-                printOutput("")
+                if(output){
+                    printHistroy(`${output} * ${history}`)
+                    printOutput("")
+                }
                break;
                case "/":
                 printHistroy(`${output} / ${history}`)
                 printOutput("")
                break;
             case "=":
-                var history = getHistory();
-                var output = getOutput();
              if(output.length && history.length){
                  printHistroy(new String (` ${history} ${output}`))
                  printOutput(eval(history + output))
@@ -80,13 +92,20 @@ document.querySelectorAll('.operator').forEach(function(item) {
 });
 
 
-let number =document.querySelectorAll(".number")
-number.forEach(item => {
-    item.addEventListener('click',function(){
-        let output = reverseNumberFormat(getOutput())
-        if(output !== NaN){
-            output= output+this.id
-            printOutput(output)
-        }
-    })
-})
+
+
+// document.querySelectorAll('.operator').forEach(item => {
+//     item.addEventListener('click', () => { 
+//         console.log("clicked");
+//         if(this.id === "clear"){
+//             console.log("clear");
+//             printHistroy("");
+//             printOutput("")
+//         } if(this.id === "backspace"){
+//             var output = reverseNumberFormat(getOutput()).toString()
+//             if(output){
+//                 output = output.substr(0, output.length-1);
+//             printOutput(output)}
+//         }
+//     })
+// })
